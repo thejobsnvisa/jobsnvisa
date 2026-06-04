6e852d151087dcb114de3cf67a214d77f4b51258
USER-28 <USER-28@growmore.local>
2026-06-04 16:11:32 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import './App.css'
 import Nav from './Components/Nav'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+
 function App() {
   return (
-    <>
-    <Nav />
     <Router>
+      <Nav />
+
       <Routes>
-        <Route path="/" element={<Home />}
-         />
+        <Route path="/" element={<Home />} />
       </Routes>
-    </Router>    
-    </>
+    </Router>
   )
 }
 
