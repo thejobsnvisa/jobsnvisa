cff26f58fe31c465b3c9cf71cacfcef635570945
USER-28 <USER-28@growmore.local>
2026-06-26 10:12:16 +0530

    project completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import logo from "../assets/logo.svg";
-import { HiMenu, HiX } from "react-icons/hi"; 
+import { HiMenu, HiX } from "react-icons/hi";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,6 +9,7 @@ const Navbar = () => {
     { name: "Home", href: "/" },
     { name: "Services", href: "#services" },
     { name: "Recruiters", href: "/recruiters" },
+    { name: "Healthcare", href: "#healthcare" },
     { name: "Job Search", href: "#jobs" },
     { name: "Blogs", href: "#blogs" },
   ];
@@ -24,12 +25,15 @@ const Navbar = () => {
     <nav aria-label="Main navigation" className="w-full">
       {/* Container switches to lg:justify-center only at 1024px+ */}
       <div className="relative w-full h-auto min-h-[100px] px-6 lg:px-10 bg-green-100 rounded-bl-[40px] rounded-br-[40px] flex items-center justify-between lg:justify-center">
-        
         {/* Logo */}
-        <img src={logo} alt="Logo" className="h-10 w-28 lg:h-12 lg:w-36 lg:absolute lg:left-20" />
+        <img
+          src={logo}
+          alt="Logo"
+          className="h-10 w-28 lg:h-12 lg:w-36 lg:absolute lg:left-20"
+        />
 
         {/* Hamburger Icon - Hidden on lg (1024px) and above */}
-        <button 
+        <button
           className="lg:hidden text-green-800 text-3xl z-50"
           onClick={() => setIsOpen(!isOpen)}
           aria-label={isOpen ? "Close menu" : "Open menu"}
@@ -40,19 +44,26 @@ const Navbar = () => {
         </button>
 
         {/* Menu - Hidden by default, flex on lg and above */}
-        <ul id="main-navigation" className={`${isOpen ? "flex" : "hidden"} lg:flex flex-col lg:flex-row absolute lg:relative top-full lg:top-0 left-0 w-full lg:w-auto bg-green-100 lg:bg-transparent py-6 px-6 lg:p-0 gap-6 lg:gap-10 items-start lg:items-center transition-all duration-300 z-40`}>
+        <ul
+          id="main-navigation"
+          className={`${isOpen ? "flex" : "hidden"} lg:flex flex-col lg:flex-row absolute lg:relative top-full lg:top-0 left-0 w-full lg:w-auto bg-green-100 lg:bg-transparent py-6 px-6 lg:p-0 gap-6 lg:gap-10 items-start lg:items-center transition-all duration-300 z-40`}
+        >
           {menuItems.map((item) => (
             <li
               key={item.name}
               className="relative"
               onMouseEnter={
                 item.name === "Services"
-                  ? () => window.matchMedia('(min-width:1024px)').matches && setIsServicesOpen(true)
+                  ? () =>
+                      window.matchMedia("(min-width:1024px)").matches &&
+                      setIsServicesOpen(true)
                   : undefined
               }
               onMouseLeave={
                 item.name === "Services"
-                  ? () => window.matchMedia('(min-width:1024px)').matches && setIsServicesOpen(false)
+                  ? () =>
+                      window.matchMedia("(min-width:1024px)").matches &&
+                      setIsServicesOpen(false)
                   : undefined
               }
             >
@@ -77,7 +88,10 @@ const Navbar = () => {
                         <a
                           href={service.href}
                           className="block whitespace-nowrap text-green-800 py-2 px-3 hover:bg-green-50 rounded"
-                          onClick={() => { setIsOpen(false); setIsServicesOpen(false); }}
+                          onClick={() => {
+                            setIsOpen(false);
+                            setIsServicesOpen(false);
+                          }}
                         >
                           {service.name}
                         </a>
